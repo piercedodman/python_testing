2516534a7a4fb547f57ecff9b28759849a228156
piercedodman <133423050+piercedodman@users.noreply.github.com>
2024-06-14 11:12:27 -0400

    Month Converter Function

--- birthday.py
+++ birthday.py
@@ -11,8 +11,10 @@ def main():
     bdayList = []
     for _ in range(int(bdays)):
         bdayList.append(randomDate())
-    print(bdayList)
-
+    
+    print(f"Here are {bdays} birthdays:")
+    for i in range(bdayList):
+        print(f"{bdayList[i][0]}")
 
 def randomDate():
     month = random.randint(1, 12)
@@ -25,6 +27,33 @@ def randomDate():
             maxday = 31
     day = random.randint(1, maxday)
     return (month, day)
+
+def monthConverter(m):
+    match m:
+        case 1:
+            return "Jan"
+        case 2:
+            return "Feb"
+        case 3:
+            return "Mar"
+        case 4:
+            return "Apr"
+        case 5:
+            return "May"
+        case 6:
+            return "Jun"
+        case 7:
+            return "Jul"
+        case 8:
+            return "Aug"
+        case 9:
+            return "Sep"
+        case 10:
+            return "Oct"
+        case 11:
+            return "Nov"
+        case 12:
+            return "Dec"
 
 if __name__ == "__main__":
     main()
